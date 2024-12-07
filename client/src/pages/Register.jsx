374f2bb7640f795c20f1459a9675b0ce4e4f1666
Nafida Nazar <nafidanazar@gmail.com>
2024-12-07 16:03:43 +0530

    updated

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { doc, setDoc } from "firebase/firestore";
 import {
-  getAuth,
   createUserWithEmailAndPassword,
   signInWithPopup,
   GoogleAuthProvider,
